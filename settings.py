2720d9dc7ee29658e5d150598dc5a8f123d117c6
DESKTOP-GPT1CVR\Lev <rameltrade@gmail.com>
2022-04-11 18:54:21 +0300

    update search algorithm

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
 settings = {
     "binance_keys":{
-        "api_key":"<api_key>",
-        "secret_key":"<secret_key>",
+        "api_key":"<api_key>",   #API ключ
+        "secret_key":"<secret_key>",    #API секретный ключ
     },
-    "telegram_token":"<telegram_token>",
-    "channel_id":"< channel_id>",
-    "chart_days":10,
-    "search_radius":12
+    "telegram_token":"<telegram_token>",  # Токен телеграм-бота
+    "channel_id":"<channel_id>",   # id канала
+    "chart_days":30,    # Количество дней в датесете
+    "search_radius":20, # Радиус экстремумов (хай/лой свечи должен быть экстремумом в этом диапазоне свеч)
+    "coin_list":[       # Список монет
+        'ETHUSDT'
+    ]
 }
