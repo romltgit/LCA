47ba2dbf74200bf2032a6237b9af2f26cf08df92
DESKTOP-GPT1CVR\Lev <rameltrade@gmail.com>
2022-04-11 18:51:53 +0300

    update search algorithm

--- csa_vds.py
+++ csa_vds.py
@@ -1,18 +1,22 @@
 from defs import *
 import json
 import datetime
-import os.path
+import os
 import time
-import telebot
-import plotly.graph_objects as go
 import threading
-last_minute = -1
 
-coin_list = [
- 'ETHUSDT','ALICEUSDT','BTCUSDT','SOLUSDT',
- 'FTMUSDT','BNBUSDT','AVAXUSDT','NEARUSDT','LUNAUSDT',
- 'XRPUSDT','DOTUSDT'
-]
+now = datetime.datetime.now()
+last_minute = int(now.minute)
+
+# Создание директорий, если их нет
+if not(os.path.isdir('dataset/bars')):
+    os.mkdir('dataset')
+    os.mkdir('dataset/bars')
+if not(os.path.isdir('ext_data')):
+    os.mkdir('ext_data')
+if not(os.path.isdir('images')):
+    os.mkdir('images')
+
 def search_coin_level_cascade(coin):
 
     global settings
@@ -23,92 +27,116 @@ def search_coin_level_cascade(coin):
     last_low = data[-1]['low']     # Последний лой
     bar = len(data)-1   # Поиск начинаеться с конца датасета
 
-    while (bar >=0 and ((len(high_array) <4 and len(low_array) <4)  or  len(high_array) < 2  or len(low_array) < 2) ):     # 
-        # поиск хаев
+    while (bar > 0 and ((len(high_array) <4 and len(low_array) <4)  or  len(high_array) < 2  or len(low_array) < 2) ):     
+        # Поиск экстремумов закончить, 
+        # как только найдется 4 экстремума одного типа, но при этом необходимо, чтобы экстремумов другого типа было найдено минимум 2
+
+        # Поиск хаев
         if(data[bar]['high'] > last_high):
             last_high = data[bar]['high']
 
             if(bar >= settings['search_radius'] and bar <= len(data) -1 - settings['search_radius']):
+
                 local_bars = data[bar - settings['search_radius']:bar + settings['search_radius']+1]
                 local_ext = True
-                for local_bar in local_bars:
+                for local_bar in local_bars:    # Проверяем, является ли хай текущей свечи экстремумом в радиусе settings['search_radius']
                     if(local_bar['high'] > last_high):
                         local_ext = False
                         break
-                if(local_ext):
+                if(local_ext):  # Если является, то добавляем его в массив экстремумов
                     high_array.append(last_high)
-        # поиск лоев
+                    
+        # Поиск лоев
         if(data[bar]['low'] < last_low):
             last_low = data[bar]['low']
 
             if(bar >= settings['search_radius'] and bar <= len(data) -1 - settings['search_radius']):
                 local_bars = data[bar - settings['search_radius']:bar + settings['search_radius']+1]
                 local_ext = True
-                for local_bar in local_bars:
+                for local_bar in local_bars:    # Проверяем, является ли лой текущей свечи экстремумом в радиусе settings['search_radius']
                     if(local_bar['low'] < last_low):
                         local_ext = False
                         break
-                if(local_ext):
+                if(local_ext):  # Если является, то добавляем его в массив экстремумов
                     low_array.append(last_low)
         bar = bar - 1
 
-    data=data[bar-10:len(data)]
+    # Обрезаем датасет до нужного размера (bar - индекс свечи, на которой закончился поиск )
+    if(bar-10 >= 0):
+        data=data[bar-10:len(data)]
+    else:
+        data=data[bar:len(data)]
 
-    ext_data = {
+    ext_data = {    
             'high_array': high_array,
             'low_array': low_array,
             'was_sent': False
     }
     path_ext = 'ext_data/' + str(coin) + '_ext.json'
 
+    # Если нет файла с экстремумами, сохраняем массивы экстремумов в файл JSON
     if(not(os.path.exists(path_ext))):
         with open(path_ext, "w") as write_file:
             json.dump(ext_data, write_file,indent=4)
 
-    # загружаем прошлые массивы экстремумов 
+    # Загружаем прошлые массивы экстремумов 
     with open(path_ext, "r") as read_file:
         previous_extremums_array = json.load(read_file)
 
-    extremums_array_changed = (ext_data['high_array'] != previous_extremums_array['high_array'] or ext_data['low_array'] != previous_extremums_array['low_array'])    # Изменились ли массивы экстремумов
-
-    if ( extremums_array_changed or (not(extremums_array_changed) and not(previous_extremums_array['was_sent'])) ): # Проверяем на каскад если изменились массивы экстремумов либо они не изменились, но не было сигнала в прошлый раз
-
-        current_price = data[-1]['close']
+    # Изменились ли массивы экстремумов
+    extremums_array_changed = (ext_data['high_array'] != previous_extremums_array['high_array'] or ext_data['low_array'] != previous_extremums_array['low_array'])    
+    
+    # Проверяем на каскад если изменились массивы экстремумов либо они не изменились, но не было сигнала в прошлый раз
+    if ( extremums_array_changed or (not(extremums_array_changed) and not(previous_extremums_array['was_sent'])) ): 
+    
+        current_price = data[-1]['close']   # Текущая цена тикера
 
         # Если до 3 экстремом вверх ближе чем до 2х вниз, то сигнал в лонг
-        if(len(ext_data['high_array']) >= 3):
-            if(abs(current_price - ext_data['high_array'][2]) < abs(current_price - ext_data['low_array'][1]) ):
+        if(len(ext_data['high_array']) >= 3 and len(ext_data['low_array']) >= 2):
+            if(abs(current_price - ext_data['high_array'][1]) < abs(current_price - ext_data['low_array'][1]) ):
                 drow_bars_and_send_telegram(data,coin,ext_data,"long")
                 ext_data['was_sent'] = True
 
         # Если до 3 экстремом вниз ближе чем до 2х вверх, то сигнал в шорт
-        if(len(ext_data['low_array']) >= 3):  
-            if(abs(current_price - ext_data['low_array'][2]) < abs(current_price - ext_data['high_array'][1]) ):
+        if(len(ext_data['low_array']) >= 3 and len(ext_data['high_array']) >= 2):  
+            if(abs(current_price - ext_data['low_array'][1]) < abs(current_price - ext_data['high_array'][1]) ):
                 drow_bars_and_send_telegram(data,coin,ext_data,"short")
                 ext_data['was_sent'] = True
         
+        # Сохраняем массивы экстремумов в файл JSON
         with open(path_ext, "w") as write_file:
             json.dump(ext_data, write_file,indent=4)
 
+connection = True   # Есть ли соединение с биржей
 while(True):
     now = datetime.datetime.now()
     minute = int(now.minute)
-    if(minute % 5 == 0 and minute != last_minute):
+    if(minute % 5 == 0 and minute != last_minute):  # Каждые 5 мин выполнять скрипт
         time.sleep(2)
-        threads = []
-        for coin in coin_list:
-            # Превышение лимита (12) потоков
-            # Из-за ограничения Binance на кол-ва запросов в секунду максимум может быть только 10 потоков для запроса данных, 1 поток основной, 1 поток получает тикеры
-            while(threading.active_count() >= 12):
-                time.sleep(0.05)
+        try:
+            status = client.get_system_status()     # Проверка статуса биржи 
+            connection = True
+        except:
+            if(connection):
+                log.error("Нет подключения к Binance")
+                connection = False
+            continue
 
-            # На каждую монету создаем поток функции get_bar
-            t = threading.Thread(target=search_coin_level_cascade, args=(coin,))
-            threads.append(t)
-            t.start()
-            # Ждем завершения работы всех потоков этой итерации
-        for t in threads:
-            t.join()
+        if(connection and not(status['status'])):   # Проверка соеденения и статуса биржи (status: (0: normal，1：system maintenance))
+            threads = []
+            for coin in settings["coin_list"]:
+                # Превышение лимита (12) потоков
+                # Из-за ограничения Binance на кол-ва запросов в секунду максимум может быть только 10 потоков для запроса данных, 1 поток основной, 1 поток получает тикеры
+                while(threading.active_count() >= 12):
+                    time.sleep(0.05)
+
+                # На каждую монету создаем поток функции search_coin_level_cascade
+                t = threading.Thread(target=search_coin_level_cascade, args=(coin,))
+                threads.append(t)
+                t.start()
+                # Ждем завершения работы всех потоков этой итерации
+            for t in threads:
+                t.join()
     last_minute = minute
     
 
--- defs.py
+++ defs.py
@@ -3,114 +3,137 @@ from settings import *
 from defs import *
 import json
 import time
-import numpy as np
 import pandas as pd
 import surrogates
 import datetime
 import os.path
 import plotly.graph_objects as go
-import plotly as plotly
-import plotly.express as px
 import copy
 import telebot
+import logging
+
+# Логирование
+logfile = 'errors.log'
+log = logging.getLogger("my_log")
+log.setLevel(logging.ERROR)
+FH = logging.FileHandler(logfile, encoding='utf-8')
+basic_formater = logging.Formatter('%(asctime)s : [%(levelname)s] : %(message)s')
+FH.setFormatter(basic_formater)
+log.addHandler(FH)
 
 # Инициализация Телеграм-бота
 bot = telebot.TeleBot(settings['telegram_token'])
 
 # Инициализация клиента Binance-api
-client = Client(settings['binance_keys']['api_key'], settings['binance_keys']['secret_key'])
+client = Client(settings['binance_keys']['api_key'], settings['binance_keys']['secret_key'],{"timeout": 5})
 
-def send_telegram(text: str,img):
-    
-    channel_id = settings['channel_id']
-    with open(img, 'rb') as photo:
-        bot.send_photo(channel_id, photo, caption=text)
+# Отправка изображений в телеграм-канал
+def send_telegram(coin,text: str,img):
+    try:
+        with open(img, 'rb') as photo:
+            bot.send_photo(settings['channel_id'], photo, caption=text)
+    except:
+        log.error(coin + ": ошибка отправки изображения")
 
-
+# Обновление или создание датасета
 def update_dataset(coin):
     global settings
+
+    # Если датасет существует
     if(os.path.exists("dataset/bars/"+ coin + ".json")):
-        with open("dataset/bars/"+ coin + ".json", "r") as read_file:
-            data = json.load(read_file)
+
+        # Загружаем датасет в массив data
+        try:
+            with open("dataset/bars/"+ coin + ".json", "r") as read_file:
+                data = json.load(read_file)
+        except:
+            log.error(coin + ": ошибка чтения датасета")
+            return
+
+        # Определяем нужные значения времени, чтобы получить недостающие свечи 
         now_time = int(time.time())
-        start_time = int(data[-1]['open_time'] + 5*60)*1000
+        start_time = int(data[-1]['open_time'] + 5*60)*1000     # К Времени открытия последней свечи в датасете прибавляем 5 минут
         end_time = int(((now_time) // 60)*60)*1000
 
+        # Если датасет очень давно не обновлялся (больше чем кол-во дней, указаных в settings['chart_days'), то меняем начальное время
         if(end_time - start_time > int(60*60*24*settings['chart_days'])):
             start_time = int(((now_time - 60*60*24*settings['chart_days']) // 60)*60*1000)
-        info = []
-        while(info == []):
-            try:
-                info = client.futures_historical_klines(coin,"5m",start_time,end_time)
-            except:
-                print(coin + ": ошибка запроса")
-        info.pop(len(info)-1)
-        for j in info:
-            open_date = int(j[0] // 1000)
-            open_date = datetime.datetime.fromtimestamp(open_date).strftime('%Y-%m-%d %H:%M:%S')
-            bar = {
-                "open_time":int(j[0] // 1000),
-                "open_time_date":open_date,
-                "open":float(j[1]),
-                "high":float(j[2]),
-                "low":float(j[3]),
-                "close":float(j[4]),
-                "volume":float(j[5]),
-                "taker_buy":float(j[9]),
-                }
-            data.append(bar)
-            data.pop(0)
-        with open("dataset/bars/"+ coin + ".json", "w") as write_file:
-            json.dump(data, write_file, indent=4)
-        return data
+            
+            # Переменная updating отвечает, необходимо ли удалять старые свечи в датасете 
+            # (это необходимо только при обновлении, чтобы в датасете были свечи только за последние settings['chart_days'] дней) 
+            updating = False
+        else:
+            updating = True
+
+    # Если датасета нет
     else:
+        # Определяем нужные значения времени, чтобы получить все свечи за последние settings['chart_days'] дней
         now_time = int(time.time())
         start_time = int(((now_time - 60*60*24*settings['chart_days']) // 60)*60*1000)
         end_time = int(((now_time) // 60)*60)*1000
-        info = []
-        while(info == []):
-            try:
-                info = client.futures_historical_klines(coin,"5m",start_time,end_time)
-            except:
-                print(coin + ": ошибка запроса")
-        info.pop(len(info)-1)
+
         data = []
-        for j in info:
-            open_date = int(j[0] // 1000)
-            open_date = datetime.datetime.fromtimestamp(open_date).strftime('%Y-%m-%d %H:%M:%S')
-            bar = {
-                "open_time":int(j[0] // 1000),
-                "open_time_date":open_date,
-                "open":float(j[1]),
-                "high":float(j[2]),
-                "low":float(j[3]),
-                "close":float(j[4]),
-                "volume":float(j[5]),
-                "taker_buy":float(j[9]),
-                }
-            data.append(bar)
+        updating = False
+
+    # Получаем данные с Binance
+    try:
+        info = client.futures_historical_klines(coin,"5m",start_time,end_time)
+    except:
+        log.error(coin + ": ошибка запроса")
+        return
+
+    # Удаляем самую новую свечку, т.к она только открылась и нам она не нужна
+    info.pop(len(info)-1)
+
+    # Добавляем свечи в датасет (массив data)
+    for j in info:
+        open_date = int(j[0] // 1000)   # Из мсек. в сек.
+        open_date_time = datetime.datetime.fromtimestamp(open_date).strftime('%Y-%m-%d %H:%M:%S')    # Из сек. в дату
+        bar = {
+            "open_time":open_date,  # Время открытия свечи (сек.)
+            "open_time_date":open_date_time, # Время открытия свечи (дата)
+            "open":float(j[1]),  
+            "high":float(j[2]),   
+            "low":float(j[3]),
+            "close":float(j[4]),
+            "volume":float(j[5]),
+            "taker_buy":float(j[9]),
+            }
+        data.append(bar)
+
+        # Если это обновление датасета, то после добавления новой свечи удаляем самую старую свечу в датасете
+        if(updating):
+            data.pop(0)
+
+    # Сохраняем обновленный (или созданный) датасет (массив data) в файл JSON
+    try:
         with open("dataset/bars/"+ coin + ".json", "w") as write_file:
             json.dump(data, write_file, indent=4)
-        return data
+    except:
+        log.error(coin + ": ошибка сохранения датасета")
+        return
+    # Возвращаем датасет для дальнейшей работы
+    return data
     
-
+# Отрисовка изображений и вызов отправки в телеграм
 def drow_bars_and_send_telegram(data,coin,ext_data,text):
-    ext_data_copy = copy.deepcopy(ext_data)
-    data_copy = copy.deepcopy(data)
 
+    # Делаем копии датасета и массивов экстремумов
+    data_copy = copy.deepcopy(data)
+    ext_data_copy = copy.deepcopy(ext_data)
+
+    # Текст сигнала
     if(text == 'long'):
         text = '%s %s Каскад уровней сверху:\n' %(surrogates.decode('\ud83d\udfe2'),coin)
-
         for high in ext_data_copy['high_array']:
             text = "{} \n {} (+{}%)".format(text,high,str(round(abs(high/data_copy[-1]['close'] - 1)*100,2)))
 
-
     elif(text == 'short'):
         text = '%s %s Каскад уровней снизу:\n' %(surrogates.decode('\ud83d\udd34'),coin)
-
         for low in ext_data_copy['low_array']:
             text = "{} \n {} (-{}%)".format(text,low,str(round(abs(low/data_copy[-1]['close'] - 1)*100,2)))
 
+    # Отрисовка свечей
     df = pd.DataFrame(data) 
     fig = go.Figure(data=[go.Candlestick(
         x=df['open_time_date'],
@@ -123,20 +146,26 @@ def drow_bars_and_send_telegram(data,coin,ext_data,text):
         )]
     )
     fig.update_layout(xaxis_rangeslider_visible=False,showlegend=False)
-    current_x_array  = []
+
+    # Отрисовка уровней
+    current_x_array  = [] 
     for bar in reversed(data_copy):
         current_x_array.append(bar['open_time_date'])
+
         if(bar['high'] in ext_data_copy['high_array']):
             fig.add_trace(go.Scatter(x=current_x_array, y=[bar['high'] for j in current_x_array], mode ="lines",line=dict(color="#B22222")))
             ext_data_copy['high_array'].remove(bar['high'])
+
         if(bar['low'] in ext_data_copy['low_array']):
             fig.add_trace(go.Scatter(x=current_x_array, y=[bar['low'] for j in current_x_array], mode ="lines",line=dict(color="#228B22")))
             ext_data_copy['low_array'].remove(bar['low'])
 
+    # Сохраняем изображение в файл и освобождаем память
     img = 'images/' + coin + '.png'
     fig.write_image(img)
     fig.data = []
     fig.layout = {}
-    
+    del fig
 
-    send_telegram(text,img)
+    # Вызов отправки изображения и текста в телеграм-канал
+    send_telegram(coin,text,img)
